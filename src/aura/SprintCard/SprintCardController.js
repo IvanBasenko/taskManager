4f884951736136e258e63b6b01dc61cc9ae1d99b
Ivan Basenko <primeivan28@gmail.com>
2018-10-31 17:27:07 +0200

    Add sub task view detail

--- src/aura/SprintCard/SprintCardController.js
+++ src/aura/SprintCard/SprintCardController.js
@@ -2,6 +2,14 @@
  * Created by Ivan Basenko on 25.10.2018.
  */
 ({
+    doInit: function (component, event, helper) {
+        let estimate = component.get('v.taskCard.Estimate__c');
+        let total = component.get('v.taskCard.Total__c');
+        if (estimate !== 0 && total !== 0) {
+            let result = (total * 100) / estimate;
+            component.set('v.progress', Math.round(result * 100) / 100);
+        }
+    },
     viewDetails: function (component, event, helper) {
         component.set('v.isOpen', true);
     },
---
+++ src/aura/TrackTimeCard/TrackTimeCardController.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Ivan Basenko on 30.10.2018.
+ */
+({
+    deleteRecord: function (component) {
+        let recordId = component.get('v.trackTime.Id');
+        let action = component.get('c.deleteById');
+        action.setParams({
+            "id": recordId
+        });
+        action.setCallback(this, function (response) {
+            if (response.getState() === 'SUCCESS') {
+
+            }
+        });
+        $A.enqueueAction(action);
+        component.getEvent('refreshSubTask').fire();
+    },
+});
