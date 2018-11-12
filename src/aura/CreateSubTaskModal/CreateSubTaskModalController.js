ea28455b587e303600d571c951754e6ec62e8164
Ivan Basenko <primeivan28@gmail.com>
2018-11-12 17:14:04 +0200

    Add check estimate for user on create sub task event

--- src/aura/CreateSubTaskModal/CreateSubTaskModalController.js
+++ src/aura/CreateSubTaskModal/CreateSubTaskModalController.js
@@ -8,4 +8,19 @@
     handleSuccess: function (component, event, helper) {
         helper.onSuccess(component);
     },
+    handleSubmit: function (component, event, helper) {
+        if (component.get('v.sprintStatus') === 'In Progress') {
+            helper.onSubmit(component, event);
+        }
+    },
+    handleChange: function (component, event, helper) {
+        if (component.get('v.sprintStatus') === 'In Progress') {
+            helper.onChange(component, event);
+        }
+    },
+    handleLoad: function (component, event, helper) {
+        if (component.get('v.sprintStatus') === 'In Progress') {
+            helper.onLoad(component, event);
+        }
+    },
 });
--- src/aura/CreateSubTaskModal/CreateSubTaskModalHelper.js
+++ src/aura/CreateSubTaskModal/CreateSubTaskModalHelper.js
@@ -2,7 +2,24 @@
  * Created by Basenko on 11.11.2018.
  */
 ({
-    //TODO change to js refresh
+    totalEstimateForUser: 0,
+    sprintEstimate: 0,
+
+    onLoad: function (component) {
+        let recordId = component.get('v.taskCardId');
+        let action = component.get('c.checkEstimateForUser');
+        action.setParams({
+            "taskId": recordId
+        });
+        action.setCallback(this, function (response) {
+            if (response.getState() === 'SUCCESS') {
+                let callback = JSON.parse(response.getReturnValue());
+                this.totalEstimateForUser = callback.totalEstimateForUser;
+                this.sprintEstimate = callback.sprintEstimate;
+            }
+        });
+        $A.enqueueAction(action);
+    },
     onSuccess: function (component) {
         let toastEvent = $A.get("e.force:showToast");
         toastEvent.setParams({
@@ -16,4 +33,21 @@
         let refreshEvent = component.getEvent("refreshSubTaskList");
         refreshEvent.fire();
     },
+    onSubmit: function (component, event) {
+        if (!component.get('v.factor')) {
+            event.preventDefault();
+            let toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                "title": "Error!!!",
+                "message": "Please change estimate on subtasks",
+                "type": "error"
+            });
+            toastEvent.fire();
+        }
+    },
+    onChange: function (component, event) {
+        let response = event.getParams('Vh').value;
+        let estimate = this.totalEstimateForUser + parseFloat(response);
+        component.set('v.factor', estimate <= this.sprintEstimate);
+    }
 });
